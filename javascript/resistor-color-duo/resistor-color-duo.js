2e6f68cee551b631ed6839cc74be9da2056104a2
Henrique Luz <riqueluz@gmail.com>
2019-11-20 10:39:38 +0100

    applying code review feedback

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,11 +3,11 @@
 // convenience to get you started writing code faster.
 //
 
-export const value = (colors) => {
+export const value = ([colorOne, colorTwo]) => {
   const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
-  const first = COLORS.indexOf(colors[0]).toString();
-  const second = COLORS.indexOf(colors[1]);
+  const first = COLORS.indexOf(colorOne).toString();
+  const second = COLORS.indexOf(colorTwo);
 
-  return parseInt(first.concat(second));
+  return Number(first.concat(second));
 };
