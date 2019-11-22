ea16a2795fa434dce4f4c780c4ea0bed8a294fcf
Henrique Luz <riqueluz@gmail.com>
2019-11-22 15:29:33 +0100

    Extracting calendar creation

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -22,17 +22,13 @@ const WEEKDAYS = {
   'Saturday' : 6,
 };
 
-export const meetupDay = (year, month, weekday, descriptor) =>  {
-  const firstDayOfMonth = new Date(year, month, 1);
-  const lastDateOfMonth = new Date(year, month + 1, 0);
-  const lastDayOfMonth = lastDateOfMonth.getDate();
-
-  let monthDays = [[], [], [], [], []];
-  let dayOfWeek = firstDayOfMonth.getDay();
+const buildMonthCalendar = (lastDayOfMonth, firstWeekDay) => {
+  let monthCalendar = [[], [], [], [], []];
+  let dayOfWeek = firstWeekDay;
   let currentWeek = 0;
 
   for (var day = 1; day <= lastDayOfMonth; day++) {
-    monthDays[currentWeek].push({ "day" : day, "dayOfWeek" : dayOfWeek });
+    monthCalendar[currentWeek].push({ "day" : day, "dayOfWeek" : dayOfWeek });
     dayOfWeek++;
 
     if ( dayOfWeek % DAYS_IN_A_WEEK === 0) {
@@ -45,7 +41,16 @@ export const meetupDay = (year, month, weekday, descriptor) =>  {
 
   }
 
-  const weekOfMonth = monthDays[WEEK_OF_MONTH[descriptor]];
+  return monthCalendar;
+}
+
+export const meetupDay = (year, month, weekday, descriptor) =>  {
+  const firstDayOfMonth = new Date(year, month, 1);
+  const lastDateOfMonth = new Date(year, month + 1, 0);
+  const lastDayOfMonth = lastDateOfMonth.getDate();
+  const firstWeekDay = firstDayOfMonth.getDay();
+  const calendar = buildMonthCalendar(lastDayOfMonth, firstWeekDay);
+  const weekOfMonth = calendar[WEEK_OF_MONTH[descriptor]];
   const meetupDay = weekOfMonth.find(week => week["dayOfWeek"] === WEEKDAYS[weekday]);
 
   return new Date(year, month, meetupDay["day"]);
