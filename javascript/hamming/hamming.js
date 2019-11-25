3f99b9b227ac9cf6f8974307f4bf0dd2d02e11d2
Henrique Luz <riqueluz@gmail.com>
2019-11-25 14:12:24 +0100

    Solving hamming problem except for empty strands

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,31 @@
+//
+// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const raiseIfSizesAreDifferent = (strand, otherStrand) => {
+  raiseIfStrandIsEmpty("left", strand);
+  raiseIfStrandIsEmpty("right", otherStrand);
+  if (strand.length !== otherStrand.length) {
+      throw new Error('left and right strands must be of equal length');
+  }
+};
+
+const raiseIfStrandIsEmpty = (strand, sequence) => {
+  if (sequence === '')
+    throw new Error(`${strand} strand must not be empty`);
+}
+
+export const compute = (left, right) => {
+  raiseIfSizesAreDifferent(left, right);
+  const strand = [];
+  const rightStrand = [...right].reverse();
+
+  [...left].forEach(letter => {
+    if (letter !== rightStrand.pop()) {
+      strand.push(letter);
+    }
+  });
+
+  return strand.length;
+};
