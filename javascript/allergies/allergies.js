90d555f133bbe559dae263a4fbfd8f0e771c1167
Henrique Luz <domingos_luz@epam.com>
2019-12-03 15:15:09 +0100

    Allergies problem base case solved

---
+++ javascript/allergies/allergies.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the 'Allergies' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const ALLERGIES = {
+  'eggs' : 1
+};
+export class Allergies {
+  constructor(score) {
+    this.score = score;
+  }
+
+  list() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  allergicTo(item) {
+    const itemScore = ALLERGIES[item];
+    return this.score > itemScore;
+  }
+}
