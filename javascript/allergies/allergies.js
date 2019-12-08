7db0cdd036253a8c720a8f1fe1445500fa340b06
Henrique Luz <riqueluz@gmail.com>
2019-12-08 19:16:18 +0100

    adding all items

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -4,7 +4,14 @@
 //
 
 const ALLERGIES = {
-  'eggs' : 1
+  'eggs' : 1,
+  'peanuts' : 2,
+  'sellfish' : 4,
+  'strawberries' : 8,
+  'tomatoes' : 16,
+  'chocolate' : 32,
+  'pollen' : 64,
+  'cats' : 128,
 };
 export class Allergies {
   constructor(score) {
