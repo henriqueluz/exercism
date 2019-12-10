90d1ef7ea857740b6e231163b38b27a1574cdb1d
Henrique Luz <domingos_luz@epam.com>
2019-12-10 16:02:40 +0100

    base case for nucleotide count problem

---
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Nucleotide-Count' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const nucleotides = ['A', 'C', 'G', 'T'];
+
+export class NucleotideCounts {
+  static parse(strand) {
+    return '0 0 0 0';
+  }
+}
