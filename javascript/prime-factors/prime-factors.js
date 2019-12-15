05ec322ea9c3e8e0a2195303c0a69bb7738f56aa
Henrique Luz <riqueluz@gmail.com>
2019-12-15 17:22:01 +0100

    getting rid of unnecessary function

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -3,25 +3,13 @@
 // convenience to get you started writing code faster.
 //
 
-const isPrime = n => {
-  const max = Math.ceil(Math.sqrt(n));
-  if (n > 1) {
-    for (let i = 2; i < max; i++) {
-      if (n % i === 0) return false;
-    }
-    return true;
-  }
-  return false;
-};
-
 export const primeFactors = number => {
   const factors = [];
   let result = number;
   let i = 2
-
   while (result > 1) {
-    if (isPrime(i) && result % i == 0) {
-      result = result / i;
+    if (result % i == 0) {
+      result /= i;
       factors.push(i);
     } else {
       i++;
