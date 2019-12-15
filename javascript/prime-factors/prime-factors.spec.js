cd9190dd62ac01e37e708ccbeac82da703f5b4f2
Henrique Luz <riqueluz@gmail.com>
2019-12-15 17:00:50 +0100

    Making all tests pass

--- javascript/prime-factors/prime-factors.spec.js
+++ javascript/prime-factors/prime-factors.spec.js
@@ -19,7 +19,7 @@ describe('primeFactors', () => {
 
   test('factors 625', () => expect(primeFactors(625)).toEqual([5, 5, 5, 5]));
 
-  xtest('factors 901255', () => expect(primeFactors(901255)).toEqual([5, 17, 23, 461]));
+  test('factors 901255', () => expect(primeFactors(901255)).toEqual([5, 17, 23, 461]));
 
-  xtest('factors 93819012551', () => expect(primeFactors(93819012551)).toEqual([11, 9539, 894119]));
+  test('factors 93819012551', () => expect(primeFactors(93819012551)).toEqual([11, 9539, 894119]));
 });
