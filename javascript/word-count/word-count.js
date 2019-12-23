9753e9c0a289c11439e1c61f4d2cb440cb9ccdf2
Henrique Luz <domingos_luz@epam.com>
2019-12-23 15:03:35 +0100

    base case for word count problem

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Word Count' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const countWords = (text) => {
+  const occurrences = {};
+  occurrences[text] = 1;
+  return occurrences;
+};
