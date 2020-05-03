4f83daceaef071b4a63c6d133ee79fe049480481
Henrique Luz <domingos_luz@epam.com>
2020-05-03 16:37:32 +0200

    Resistor color problem solution for Ruby Track

---
+++ ruby/resistor-color/resistor_color.rb
@@ -0,0 +1,6 @@
+module ResistorColor
+  COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+  def self.color_code(color)
+    COLORS.index(color);
+  end
+end
